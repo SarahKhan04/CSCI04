8dae8e50ee611276ad1994a9d945e748110e4aff
SarahKhan04 <skhan026@student.butte.edu>
2025-02-04 09:05:21 -0800

    Update day1.py

--- day1.py
+++ day1.py
@@ -1,1 +1,14 @@
+devil = 2531
+Haha = devil +1
+e = 5**5
+Squid = "word"
+a = 2.5
+col = [ 2, 3, 4, 5, 6, 1]
+add = sum (num1,num2)
+num1 = 1
+num2= 2
+def add (num1,num2):
+  return sum (num1,num2)
+  
+
 
